2d55a33f8149266af877042e444c660c58410243
Zenepity <jacksonw@gmx.us>
2022-08-06 19:29:08 -0400

    :sparkles: Chores

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    port: 4000,
+  },
+  preview: {
+    port: 4000,
+  },
+});
